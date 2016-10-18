2f5d1dbc9a240dc515301f44e9854e8ccbbe4722
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-18 11:04:06 -0700

    Set up initial content, css styling, jQuery toggles.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+  $("#castle-click").click(function() {
+    $("#castles").slideToggle();
+  });
+  $("#sakura-click").click(function() {
+    $("#sakura").slideToggle();
+  });
+  $("#island-click").click(function() {
+    $("#islands").slideToggle();
+  });
+  $("#shinkansen-click").click(function() {
+    $("#shinkansen").slideToggle();
+  });
+});
